41086ba7dd6a83634b3abf0342fde51a3124d3df
prasurjya <65493249+PrasurjyaWhiteHatJr@users.noreply.github.com>
2021-06-05 13:14:34 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.express as px
+df = pd.read_csv("data.csv")
+fig = px.scatter(df, x = "Population", y = "Per capita", size = "Percentage", color = "Country", size_max = 60)
+fig.show()
+
+# import pandas as pd
+# import plotly.express as px
+# df = pd.read_csv("line_chart.csv")
+# fig = px.line(df, x = "Year", y = "Per capita income", color = "Country", title = "Per capita income")
+# fig.show()
+
+# import pandas as pd
+# import plotly.express as px
+# df = pd.read_csv("line_chart.csv")
+# fig = px.bar(df, x = "Year", y = "Per capita income", color = "Country", title = "Per capita income")
+# fig.show()
+
+# import pandas as pd
+# import plotly.express as px
+# df = pd.read_csv("data.csv")
+# fig = px.bar(df, x = "Country", y = "Internetusers")
+# fig.show()
